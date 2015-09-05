629f88450112f823bc30a4e50c9ba2ecb89f6733
Nguyen Ngoc Duc <ducnn01@gmail.com>
2015-09-06 01:22:18 +0700

    cm

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/*router.get('/', function(req, res, next) {
+ res.render('index', { title: 'Express' });
+ });*/
+
+router.get('/', function (req, res) {
+    res.render('index', {title: "Home page"});
+
+});
+
+router.post('/login', function (req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
+    /*
+     Perform the login and send a response.
+     */
+    res.json({"done": "yes"});
+    //OR
+    res.render('account');
+});
+
+router.get('/profile', function (req, res) {
+    var profile_id = req.query.id;
+    res.render('profile', {id: profile_id});
 });
 
 module.exports = router;
